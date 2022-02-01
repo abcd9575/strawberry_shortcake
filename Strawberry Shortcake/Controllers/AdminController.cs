3572eb60aebef22e38132693c65745297c202e5a
Tachyon <z16844@icloud.com>
2022-01-31 19:12:17 -0500

    Admin에서 회원등급 설정 추가

--- Strawberry Shortcake/Controllers/AdminController.cs
+++ Strawberry Shortcake/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,14 +13,35 @@ using BC = BCrypt.Net.BCrypt;
 
 namespace Strawberry_Shortcake.Controllers
 {
-    [Authorize(Roles="Administrator")]
+    [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        /* TODO
+         * 
+         * - 회원 등급 상수화 (코딩하기 쉽게)
+         */
         private readonly Strawberry_ShortcakeContext db;
 
         public AdminController(Strawberry_ShortcakeContext context)
         {
             db = context;
+        }
+        private List<SelectListItem> GetAvailableRoles(User user = null)
+        {
+            /* 지금 로그인 한 계정의 회원등급 가져오기 */
+            var CurrentRoleName = User.Claims.Single(r => r.Type == ClaimTypes.Role).Value;
+            var CurrentRole = db.Roles.SingleOrDefault(r => r.RoleName.Equals(CurrentRoleName));
+
+            return db.Roles.ToList().Select(r => new SelectListItem
+            {
+                Text = r.RoleName,
+                Value = r.Id.ToString(),
+                Selected = user == null ? r.Id == 1 : user?.RoleId == r.Id,
+                Disabled = 
+                    r.RoleName.Equals("Administrator") // 새로운 관리자 계정 만들기 금지
+                    && user?.Role?.RoleName?.Equals("Administrator") != true // 지금 로그인한 계정이 관리자 계정이 아닐 경우
+                    && r.Id >= CurrentRole.Id, // 지금 로그인 한 계정보다 높은 등급을 설정 못하게 방지
+            }).ToList();
         }
 
         // GET: Admin
@@ -49,7 +71,10 @@ namespace Strawberry_Shortcake.Controllers
         // GET: Admin/Create
         public IActionResult Create()
         {
-            return View();
+            UserViewModel model = new UserViewModel {
+                Role = GetAvailableRoles()
+            };
+            return View(model);
         }
 
         // POST: Admin/Create
@@ -57,7 +82,7 @@ namespace Strawberry_Shortcake.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserNo,UserEmail,UserPw,UserName,Activation")] User user)
+        public async Task<IActionResult> Create([Bind("UserNo,UserEmail,UserPw,UserName,Activation,RoleId")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -82,7 +107,16 @@ namespace Strawberry_Shortcake.Controllers
             {
                 return NotFound();
             }
-            return View(user);
+            UserViewModel model = new UserViewModel() {
+                UserName= user.UserName,
+                UserNo = user.UserNo,
+                UserEmail = user.UserEmail,
+                Activation = user.Activation,
+                UserPw = user.UserPw,
+                RoleId = user.RoleId,
+                Role = GetAvailableRoles(user)
+            };
+            return View(model);
         }
 
         // POST: Admin/Edit/5
@@ -90,7 +124,7 @@ namespace Strawberry_Shortcake.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserNo,UserEmail,UserPw,UserName,Activation")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("UserNo,UserEmail,UserPw,UserName,Activation,RoleId")] User user)
         {
             if (id != user.UserNo)
             {
---
+++ Strawberry Shortcake/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Strawberry_Shortcake.Models
+{
+    public class UserViewModel: User
+    {
+        public new int RoleId;
+        public new List<SelectListItem> Role { get; set; }
+    }
+}

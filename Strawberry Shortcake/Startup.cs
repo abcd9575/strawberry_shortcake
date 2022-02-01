474e01ef262e38bf405923fd725a1bbc2ca004de
Tachyon <z16844@icloud.com>
2022-01-31 19:10:44 -0500

    html파일 수정되면 자동 업데이트

--- Strawberry Shortcake/Startup.cs
+++ Strawberry Shortcake/Startup.cs
@@ -27,7 +27,7 @@ namespace Strawberry_Shortcake
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddSession(); // 서비스에 등록함
 
             services.AddDbContext<Strawberry_ShortcakeContext>(options =>
